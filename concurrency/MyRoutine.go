135331dc45d9fafd13e70879a5b0291f05d26468
hoanglong2534 <phamxuanhoanglong@gmail.com>
2026-02-07 10:16:15 +0700

    concurrency - channel

--- concurrency/MyRoutine.go
+++ concurrency/MyRoutine.go
@@ -4,16 +4,36 @@ import (
 	"fmt"
 )
 
-func sayHi() {
+func sayHi(channel chan string) {
 	fmt.Println("Hi")
+
+	channel <- "sayHi da chay xong!"
 }
 
 func MyRoutine() {
-	go sayHi()
+
+	myChannel := make(chan string, 2)
+
+	myChannel <- "ptit"
+
+	go sayHi(myChannel)
+
 	fmt.Println("Long")
+
+	fmt.Println(<-myChannel)
+	fmt.Println(<-myChannel)
 
 	//output:
 	// Long
-	//Không in ra Hi vì goroutine sayHi có thể chưa được go lập lịch để chạy
-	//trong khi MyRoutine đã kết thúc làm process kết thúc theo.
+	//ptit
+	//Hi
+	//sayHi da chay xong!
+
+	// myChannel có size = 2, ban đầu đẩy "ptit" vào queue , sau đó chạy sayHi ở 1 go routine khác, tiếp tục
+	// chạy in ra "Long" ,
+	// in ra "ptit"
+	// sau đó hàm sayHi chạy xong in ra Hi, và đẩy tiếp "sayHi da chay xong!" vào queue
+	// in từ queuer	ra "sayHi da chay xong!"
+
+	// tùy CPU lập lịch mà "ptit" và "Hi" có thể in ra trước hoặc sau nhau
 }
